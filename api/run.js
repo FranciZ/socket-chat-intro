d843c4c0249446250ca86387830bbff7344cce17
FranciZ <franci@proxima.si>
2016-06-08 15:33:33 +0200

    fix

--- api/run.js
+++ api/run.js
@@ -4,7 +4,7 @@ const app = express();
 
 
 const server    = require('http').Server(app);
-const io        = require('socket.io')(server);
+const io        = require('socket.io')(server, { path: '/api/socket.io'});
 const cors      = require('cors');
 const bodyParser = require('body-parser');
 
